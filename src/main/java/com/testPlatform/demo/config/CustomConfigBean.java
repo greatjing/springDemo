a876328f0615ec4e378e348febdd8296d2cb7dca
greatjing <greatjing@sina.cn>
2017-09-07 16:44:38 +0800

    custom properties exercise

---
+++ src/main/java/com/testPlatform/demo/config/CustomConfigBean.java
@@ -0,0 +1,10 @@
+package com.testPlatform.demo.config;/**
+ * Created by guojingjing on 7/9/17.
+ */
+
+/**
+ ** @description $
+ ** Created by guojingjing on 7/9/17. 
+ **/
+public class customConfigBean {
+}
